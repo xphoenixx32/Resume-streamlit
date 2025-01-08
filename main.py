24f6632678ce469b6007dab55d8013d654bb4989
Lean Lin <xphoenixx32@gmail.com>
2025-01-08 16:05:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ st.caption('''
 # st.image("assets/profile.png", width = 160)
 st.title(f'''***{WORK_NAME}***''')
 st.caption(f'''***{TC_NAME}***''')    
-# st.warning(DESCRIPTION_a, icon = "🤖")
+st.caption(f'''***{DESCRIPTION_a}***''')
 st.success(DESCRIPTION_b, icon = "💡")
 
 st.download_button(
